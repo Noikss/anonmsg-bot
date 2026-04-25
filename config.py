b19d968de0d3fab55b81cfd2584e58bff8dcc4b1
Noikss <albinaandreevna1972@gmail.com>
2026-04-25 04:27:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("8468469147:AAEyIFUXSvcHm1riOflvh1X9RyWmOryieeY")
-ADMIN_ID = int(os.getenv("7360025537", 0))
+BOT_TOKEN = "8468469147:AAEyIFUXSvcHm1ri0flvh1X9RyWmOryieeY"
+ADMIN_ID = 7360025537
